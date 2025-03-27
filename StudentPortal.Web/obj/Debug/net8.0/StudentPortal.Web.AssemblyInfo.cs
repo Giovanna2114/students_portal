0e7d7ace3544fecd55f628db23e096f0feb554c2
Giovanna Teixeira <giovanna.teixeira563@gmail.com>
2025-03-27 00:05:00 -0300

    Finalização da primeira versão do projeto

--- StudentPortal.Web/obj/Debug/net8.0/StudentPortal.Web.AssemblyInfo.cs
+++ StudentPortal.Web/obj/Debug/net8.0/StudentPortal.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentPortal.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8ab8efdaf24bd4a7c8ce9c8853cc89548a04ea8")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentPortal.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentPortal.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
